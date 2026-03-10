51ffc4e701c208099a89938a28ac8825868a87d5
raghav(raghava-0650) <raghav83286@gmail.com>
2026-03-10 23:25:24 +0530

    dashboard layout updated

---
+++ app/dashboard/_components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { UserButton } from '@clerk/nextjs';
+
+function Header() {
+  return (
+    <div className='flex justify-end p-5 shadow-sm'>
+        <UserButton/>
+    </div>
+  )
+}
+
+export default Header
---
+++ app/dashboard/layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Header from './_components/Header';
+import SideBar from './_components/SideBar';
+
+function DashboardLayout({children}) {
+  return (
+    <div>
+        <div className=' md:w-64 h-screen fixed'>
+            <SideBar/>
+        </div>
+        <div className='md:ml-64'>
+            <Header/>
+            <div className='p-10'>
+                {children}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
